8683794cbff38dc730be7bb3f4c3054c2add0c6e
ninja3011 <ninadjangle3011@gmail.com>
2022-02-19 13:40:13 +0530

    LiveChat

--- src/menu-items/dashboard.js
+++ src/menu-items/dashboard.js
@@ -18,6 +18,14 @@ const dashboard = {
             url: '/dashboard/default',
             icon: icons.IconDashboard,
             breadcrumbs: false
+        },
+        {
+            id: 'LiveChat',
+            title: 'LiveChat',
+            type: 'item',
+            url: '/LiveChat',
+            icon: icons.IconDashboard,
+            breadcrumbs: false
         }
     ]
 };
